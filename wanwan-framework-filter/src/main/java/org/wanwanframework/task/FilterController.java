499c7cdacc0db304cfcb70981eb0e02bdf5b9367
wanwang2 <1053400545@qq.com>
2017-03-29 16:51:28 +0800

    .

---
+++ wanwan-framework-filter/src/main/java/org/wanwanframework/task/FilterController.java
@@ -0,0 +1,13 @@
+package org.wanwanframework.task;
+
+public class FilterController {
+	
+	public void init() {
+		System.out.println("init...");
+	}
+	
+	public static void main(String[] args) {
+		FilterController check = new FilterController();
+		check.init();
+	}
+}
---
+++ wanwan-framework-filter/src/test/java/org/wanwanframework/task/TimerTest.java
@@ -0,0 +1,5 @@
+package org.wanwanframework.task;
+
+public class TimerTest {
+
+}
