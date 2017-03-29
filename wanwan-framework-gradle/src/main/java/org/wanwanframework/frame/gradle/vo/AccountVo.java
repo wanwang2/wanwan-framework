4617b5fe1a6d1be1db047d67b7ed88f9677b8e4f
wanwang2 <1053400545@qq.com>
2017-03-29 14:02:23 +0800

    .

--- wanwan-framework-gradle/src/main/java/org/wanwanframework/frame/gradle/vo/AccountVo.java
+++ wanwan-framework-gradle/src/main/java/org/wanwanframework/frame/gradle/vo/AccountVo.java
@@ -2,4 +2,22 @@ package org.wanwanframework.frame.gradle.vo;
 
 public class AccountVo {
 
+	private String username;
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
