90bfad29013bebaa8524ccdba3965dda5bd7c711
wanwang2 <1053400545@qq.com>
2017-05-09 14:32:31 +0800

    ...

---
+++ wanwan-framework-jersey/src/main/java/org/wanwanframework/jersey/JerseyRemoteFacade.java
@@ -0,0 +1,10 @@
+package org.wanwanframework.jersey;
+
+import java.util.Map;
+
+public class JerseyRemoteFacade {
+
+	public void invoke(String uri, String method, Map<String, String> param, Class<?> clazz) {
+	
+	}
+}
