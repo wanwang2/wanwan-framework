fdacc796477614091c852c64d8474fa0f60b0305
wanwang2 <1053400545@qq.com>
2017-06-29 15:24:42 +0800

    ...

--- jersey/wanwan-jersey-spring/src/test/java/org/wanwanframework/jersey/spring/DataSourceTest.java
+++ jersey/wanwan-jersey-spring/src/test/java/org/wanwanframework/jersey/spring/DataSourceTest.java
@@ -1,6 +1,6 @@
+package org.wanwanframework.jersey.spring;
 
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
