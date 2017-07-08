d0a49182583125cc24017269057f20f0e4977d4d
wanwang2 <1053400545@qq.com>
2017-07-08 18:24:46 +0800

    ..add RedisTemplate

---
+++ jersey/wanwan-jersey-spring/src/test/java/org/wanwanframework/jersey/spring/RedisTemplateTest.java
@@ -0,0 +1,26 @@
+package org.wanwanframework.jersey.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/context-redis.xml" })
+public class RedisTemplateTest {
+
+	@Autowired
+	RedisTemplate<String, Object> redisTemplate;
+
+	@Test
+	public void testRedis() {
+
+		ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+		valueOperations.set("vv", "123456789");
+		String value = (String)valueOperations.get("vv");
+		System.out.println("value:" + value);
+	}
+}
